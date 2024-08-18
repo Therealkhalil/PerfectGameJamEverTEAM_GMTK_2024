2b05f74d2c8fb0e27a97b2f2b64fa6d93da511a9
rcfieldhouse <rcfieldhouse@yahoo.com>
2024-08-18 18:06:58 -0400

    shatterable boxes

--- 3DPlatformer_GMTK2024/Assets/Scripts/ShatterObject.cs
+++ 3DPlatformer_GMTK2024/Assets/Scripts/ShatterObject.cs
@@ -4,57 +4,59 @@ using UnityEngine;
 
 public class ShatterObject : MonoBehaviour
 {
+    //The object's current health point total
     [SerializeField]
     private GameObject brokenPrefab;
     public bool UseAdditionalUpForce = false;
     public float shatterForce = 10f;
+    private float[] randoms = { 0, 0.1f, 0.2f, 0.3f, 0.4f, 0.5f, 0.6f, 0.7f, 0.8f, 0.9f };
     bool Shattered = false;
 
-    public void HandleObjectDeath(Transform context)
+ 
+    public void ShatterObj(Transform context)
     {
         if (Shattered == true)
             return;
 
         Shattered = true;
-        //Possibly don't need to check for this breakable tag :3
-        foreach (BoxCollider box in gameObject.GetComponents<BoxCollider>())
-        {
-            box.enabled = false;
-        }
         foreach (BoxCollider box in gameObject.GetComponents<BoxCollider>())
         {
             box.enabled = false;
         }
         Debug.Log("Handle Object Death called from " + context.name);
-
-        //create our broken object and reparent it (pretty expensive tbh)
+        //create our broken object and reparent it
         GameObject newObject = Instantiate(brokenPrefab, transform.position, transform.rotation);
 
         int index = 0;
-
         //iterate through children and apply a force
         foreach (Rigidbody rb in newObject.GetComponentsInChildren<Rigidbody>())
         {
 
-            //This would normally explode radially, but because the object transform and the context are not in the same position
-            //it favors 1 direction
             Vector3 force = (rb.transform.position - context.transform.position).normalized * shatterForce;
 
             float rand = Random.Range(0, 1);
 
             if (UseAdditionalUpForce == true)
-                rb.velocity = Vector3.up * (shatterForce / 10.0f) * rand;
+                rb.velocity = Vector3.up * (shatterForce / 10.0f) * randoms[index];
 
             if (index == 9)
                 index = 0;
 
             rb.AddForce(force);
-
-            //This is for a shader that dissolves/destroys the children after a short amount of time
-            //rb.gameObject.AddComponent<DissolveRock>();
             index++;
         }
-        //TODO: Object pooling
+       
+
+   
         Destroy(gameObject);
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("hit");
+    }
+    private void Awake()
+    {
+        //ShatterObj(transform);
     }
 }

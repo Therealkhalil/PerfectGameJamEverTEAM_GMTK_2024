eb2edbdccf661b8f492c5f92c58e72909a3794c4
Oroppo <dante.arruda416@gmail.com>
2024-08-20 05:54:23 -0400

    Saved Prototype Scene

--- 3DPlatformer_GMTK2024/Assets/WinCondition.cs
+++ 3DPlatformer_GMTK2024/Assets/WinCondition.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using FMODUnity;
 
 public class WinCondition : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Transform player;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.transform.CompareTag("Player"))
+        {
+            GameManager.instance.SceneChangeAsync("Cutscene End");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3.Distance(transform.position, player.position);
+
     }
 }

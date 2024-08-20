427127fa792ebaad8865dbf87b0970fb5585a4f4
Khalil Chabir <khalil.chabir96@gmail.com>
2024-08-20 15:01:42 +0100

    begin cutscene

--- 3DPlatformer_GMTK2024/Assets/GameManager.cs
+++ 3DPlatformer_GMTK2024/Assets/GameManager.cs
@@ -15,9 +15,6 @@ public class GameManager : MonoBehaviour
     [Header("Loading Screen")]
     public GameObject loadingScreen;
     public Image loadingBarFill;
-    [SerializeField] public TextMeshProUGUI loadScreenText;
-    bool renderLoadScreen = true;
-    bool playSelected;
 
     [Header("Time Slow")]
     public float timeSlowStrength = 0.05f;
@@ -61,20 +58,17 @@ public class GameManager : MonoBehaviour
 
         SceneManager.activeSceneChanged += ChangedActiveScene;
     }
-    private void Start()
-    {
-        //Checking Scenes
-        if (SceneManager.GetActiveScene().name == "Cutscene Begin")
-        {
-            SceneChange(SceneManager.GetActiveScene().buildIndex + 1);
-        }
-    }
 
+    
 
     private void ChangedActiveScene(Scene arg1, Scene arg2)
     {
+
         Invoke("MusicSelect", .1f);
+
     }
+
+    
 
     private void MusicSelect()
     {
@@ -127,8 +121,9 @@ public class GameManager : MonoBehaviour
         Debug.Log("Called");
         yield return new WaitForSeconds(Time);
 
+        //GameObject.Find("DataPersistenceManager").GetComponent<DataPersistenceManager>().SaveGame();
+
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
-        operation.allowSceneActivation = false;
 
         loadingScreen.SetActive(true);
 
@@ -141,7 +136,7 @@ public class GameManager : MonoBehaviour
         loadingScreen.SetActive(false);
         Debug.Log("New scene loaded");
 
-
+        //GameObject.Find("DataPersistenceManager").GetComponent<DataPersistenceManager>().LoadGame();
     }
     public void SceneChange(string sceneName, float Time)
     {
@@ -160,9 +155,9 @@ public class GameManager : MonoBehaviour
     public IEnumerator SceneChangeAsync(int sceneID)
     {
         AsyncOperation operation = SceneManager.LoadSceneAsync(sceneID);
-        operation.allowSceneActivation = false;
+        
 
-        if (loadingScreen == null)
+        if(loadingScreen == null)
         {
             while (!operation.isDone)
             {
@@ -175,31 +170,15 @@ public class GameManager : MonoBehaviour
 
         while (!operation.isDone)
         {
-            if (renderLoadScreen)
-            {
-                float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
-                loadingBarFill.fillAmount = progressValue;
-
-                if (loadScreenText)
-                {
-                    loadScreenText.text = progressValue.ToString() + "%";
-                }
-            }
-           
-            yield return null;
-
-            if (loadingScreen)
-                loadingScreen.SetActive(false);
-
-        }
-        if (playSelected)
-        {
-            operation.allowSceneActivation = true;
-        }
-            
+            float progressValue = Mathf.Clamp01(operation.progress / 0.9f);
+            loadingBarFill.fillAmount = progressValue;
+            yield return null;      
         }
 
-        
+        if(loadingScreen)
+            loadingScreen.SetActive(false);
+        //GameObject.Find("DataPersistenceManager").GetComponent<DataPersistenceManager>().LoadGame();
+    }
     public IEnumerator SceneChangeAsync(string sceneName)
     {
         Debug.Log("new scene");
